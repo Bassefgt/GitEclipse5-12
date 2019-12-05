dd8c4d78909f9c5b5be0aea2b03781125c6c5c3c
MarioBasse <marioga1777@gmail.com>
2019-12-05 11:02:02 +0100

    Descomentamos todo

--- src/ejercicios/Metodos.java
+++ src/ejercicios/Metodos.java
@@ -4,15 +4,15 @@ public class Metodos {
 
 	public static void main(String[] args) {
 		
-//		String v1 = "LeBron " ;
-//		String v2 = "James";
-//		System.out.println(lakers(v1, v2));
-//		
-//		int numero = 76;
-//		System.out.println(ascii(numero));
-//		
-//		char norChar = '$';
-//		System.out.println(normal(norChar));
+		String v1 = "LeBron " ;
+		String v2 = "James";
+		System.out.println(lakers(v1, v2));
+		
+		int numero = 76;
+		System.out.println(ascii(numero));
+		
+		char norChar = '$';
+		System.out.println(normal(norChar));
 		
 		int num1 = 60;
 		int num2 = 3;
@@ -21,28 +21,28 @@ public class Metodos {
 	}
 	
 	
-//	public static String lakers(String v1, String v2) {
-//		
-//		String vt = v1 + v2;
-//		
-//		return vt;
-//	}
-//
-//	
-//	public static char ascii(int numero) {
-//		
-//		char numAscii = (char) numero;
-//		
-//		return numAscii;
-//	}
-//
-//	
-//	public static int normal(char norChar)  {
-//		
-//		int numerito = (int) norChar;
-//		
-//		return numerito;
-//	}
+	public static String lakers(String v1, String v2) {
+		
+		String vt = v1 + v2;
+		
+		return vt;
+	}
+
+	
+	public static char ascii(int numero) {
+		
+		char numAscii = (char) numero;
+		
+		return numAscii;
+	}
+
+	
+	public static int normal(char norChar)  {
+		
+		int numerito = (int) norChar;
+		
+		return numerito;
+	}
 	
 	
 	public static int cuentaAtras(int numMain, int numCuenta) {
