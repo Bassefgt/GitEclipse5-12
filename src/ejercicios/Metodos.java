eaebbe85a9c07d4c23c3f5be93444a09f2bcc240
MarioBasse <marioga1777@gmail.com>
2019-12-05 10:44:50 +0100

    Añadimos rama de la cuenta atrás

--- src/ejercicios/Metodos.java
+++ src/ejercicios/Metodos.java
@@ -4,38 +4,56 @@ public class Metodos {
 
 	public static void main(String[] args) {
 		
-		String v1 = "LeBron " ;
-		String v2 = "James";
-		System.out.println(lakers(v1, v2));
+//		String v1 = "LeBron " ;
+//		String v2 = "James";
+//		System.out.println(lakers(v1, v2));
+//		
+//		int numero = 76;
+//		System.out.println(ascii(numero));
+//		
+//		char norChar = '$';
+//		System.out.println(normal(norChar));
 		
-		int numero = 76;
-		System.out.println(ascii(numero));
-		
-		char norChar = '$';
-		System.out.println(normal(norChar));
+		int num1 = 60;
+		int num2 = 3;
+			
+		cuentaAtras(num1, num2);
 	}
 	
 	
-	public static String lakers(String v1, String v2) {
-		
-		String vt = v1 + v2;
-		
-		return vt;
-	}
-
+//	public static String lakers(String v1, String v2) {
+//		
+//		String vt = v1 + v2;
+//		
+//		return vt;
+//	}
+//
+//	
+//	public static char ascii(int numero) {
+//		
+//		char numAscii = (char) numero;
+//		
+//		return numAscii;
+//	}
+//
+//	
+//	public static int normal(char norChar)  {
+//		
+//		int numerito = (int) norChar;
+//		
+//		return numerito;
+//	}
 	
-	public static char ascii(int numero) {
-		
-		char numAscii = (char) numero;
-		
-		return numAscii;
-	}
-
 	
-	public static int normal(char norChar)  {
+	public static int cuentaAtras(int numMain, int numCuenta) {
+			
+		do {
+			
+			numMain = numMain - numCuenta;
+			System.out.println(numMain);
+			
+		} while (numMain > 0);
 		
-		int numerito = (int) norChar;
-		
-		return numerito;
+		return numMain;
 	}
 }
