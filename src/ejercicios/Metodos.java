98bf1c3f92e8b8aada7d3f9ea16775338cc824da
MarioBasse <marioga1777@gmail.com>
2019-12-05 09:18:42 +0100

    Modifico el ejercicio

--- src/ejercicios/Metodos.java
+++ src/ejercicios/Metodos.java
@@ -3,8 +3,20 @@ package ejercicios;
 public class Metodos {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		String v1 = "LeBron " ;
+		String v2 = "James";
+		
+		System.out.println(lakers(v1, v2));
+		
+	}
+	
+	
+	public static String lakers(String v1, String v2) {
+		
+		String vt = v1 + v2;
+		
+		return vt;
 	}
 
 }
